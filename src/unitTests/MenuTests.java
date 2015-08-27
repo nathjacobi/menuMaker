e34dfff0e96f73944ac0c50ea3e8820595b2d5b3
Nathaniel Jacobi <njacobi@mines.edu>
2015-08-27 10:53:26 -0600

    Moved the test recipes to the @Before function to get rid of the repeats.

--- src/unitTests/MenuTests.java
+++ src/unitTests/MenuTests.java
@@ -9,79 +9,66 @@ import menuMaker.*;
 public class MenuTests {
 	private static Menu mainMenu;
 	
+	private static Recipe milkChickenRecipe;
+	private static Recipe bbqChicken;
+	private static Recipe beefRoast;
+	
 	@Before
 	public void setup() {
 		mainMenu = new Menu();
-	}
-	
-	@Test
-	public void checkMenuFilling(){
-		Recipe milkChickenRecipe = new Recipe("Milk Chicken", 2);
-		Recipe bbqChicken = new Recipe("BBQ Chicken", 1);
-		Recipe beefRoast = new Recipe("Beef Roast", 3);
-		
-		mainMenu.setAllowRepeats(true);
-		
+
+		milkChickenRecipe = new Recipe("Milk Chicken", 2);
+		bbqChicken = new Recipe("BBQ Chicken", 1);
+		beefRoast = new Recipe("Beef Roast", 3);
+
 		mainMenu.addRecipe(milkChickenRecipe);
 		mainMenu.addRecipe(beefRoast);
 		mainMenu.addRecipe(bbqChicken);
-		
+	}
+
+	@Test
+	public void checkMenuFilling(){
+		mainMenu.setAllowRepeats(true);
+
 		int testValue1 = 14;
 		int testValue2 = 10;
 		int testValue3 = 7;
-		
+
 		mainMenu.fillMenu(testValue1);
 		assertTrue(mainMenu.getDaysCovered() == testValue1);
-		
+
 		mainMenu.clearMenu();
-		
+
 		mainMenu.fillMenu(testValue2);
 		assertTrue(mainMenu.getDaysCovered() == testValue2);
-		
+
 		mainMenu.clearMenu();
-		
+
 		mainMenu.fillMenu(testValue3);
 		assertTrue(mainMenu.getDaysCovered() == testValue3);
 	}
-	
+
 	@Test
-	public void checkRecipeAdded(){
-		Recipe milkChickenRecipe = new Recipe("Milk Chicken", 2);
-		Recipe bbqChicken = new Recipe("BBQ Chicken", 1);
-		Recipe beefRoast = new Recipe("Beef Roast", 3);
-		
+	public void checkRecipeAdded(){		
 		mainMenu.setAllowRepeats(false);
-		
-		mainMenu.addRecipe(milkChickenRecipe);
-		mainMenu.addRecipe(beefRoast);
-		mainMenu.addRecipe(bbqChicken);
-		
+
 		mainMenu.fillMenu(6);
 		assertTrue(mainMenu.getMenuList().contains(milkChickenRecipe));
 		assertTrue(mainMenu.getMenuList().contains(bbqChicken));
 		assertTrue(mainMenu.getMenuList().contains(beefRoast));
-		
+
 	}
-	
+
 	@Test
 	public void checkMenuCoversDaysWithRepeats() {
-		Recipe milkChickenRecipe = new Recipe("Milk Chicken", 2);
-		Recipe bbqChicken = new Recipe("BBQ Chicken", 1);
-		Recipe beefRoast = new Recipe("Beef Roast", 3);
-		
-		
-		mainMenu.addRecipe(milkChickenRecipe);
-		mainMenu.addRecipe(beefRoast);
-		mainMenu.addRecipe(bbqChicken);
-		
 		int testValue1 = 5;
 		int testValue2 = 4;
-		
+
 		mainMenu.setAllowRepeats(true);
-		
+
 		mainMenu.fillMenu(testValue1);
 		assertTrue(testValue1 == mainMenu.getDaysCovered());
-		
+
 		mainMenu.fillMenu(testValue2);
 		assertTrue(testValue2 == mainMenu.getDaysCovered());
 	}
